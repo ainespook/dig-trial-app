d49d8b57bbdfa496e4f19e58509db9048f7fecb3
MohamedAltooq2 <m.altooq1@universityofgalway.ie>
2024-12-02 23:49:47 +0000

    I start by add selectInput for Treatment Placebo and Treatment

--- app.R
+++ app.R
@@ -1,11 +1,20 @@
 #this is a basic template that we can work from, it can be edited as we need
+
+if (!require("shiny")) install.packages("shiny")
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("palmerpenguins")) install.packages("palmerpenguins")
+if (!require("DynNom")) install.packages("DynNom")
+if (!require("shinydashboard")) install.packages("shinydashboard")
+library(tidyverse)
 library(shiny)
+library(palmerpenguins)
+
 
 ui <- fluidPage(
   titlePanel("DIG Trial Data Exploration"),
   sidebarLayout(
     sidebarPanel(
-      #inputs here
+      selectInput(inputId = "TRTMT", label = "Treatment Group:", choices = c("Placebo", "Treatment"), multiple = FALSE)
     ),
     mainPanel(
       #outputs here
